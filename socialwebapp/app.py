6cdc01b93b9c441400d824af935a81e8db6fe5a9
Bech <tobechionwuka@gmail.com>
2021-01-23 11:03:49 -0500

    initial commit - base of webpage

--- socialwebapp/app.py
+++ socialwebapp/app.py
@@ -4,8 +4,10 @@ app = Flask(__name__)  #creates an instance of flask
 
 @app.route('/')
 def index():
-    return render_template("index.html") #renders template from template folder
+    title = "Act On It!"
+    return render_template("index.html", title=title) #renders template from template folder
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
