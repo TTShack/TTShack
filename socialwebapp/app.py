5074588b77e189bb083f12db83b003370bac7a75
Bech <tobechionwuka@gmail.com>
2021-01-24 00:15:51 -0500

    initial commit

--- socialwebapp/app.py
+++ socialwebapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)  #creates an instance of flask
 
@@ -13,3 +13,14 @@ def about():
 @app.route('/joinorg')
 def joinorg():
     return render_template("joinorg.html")
+
+@app.route('/hsorgs')
+def hsorgs():
+    return render_template("hsorgs.html")
+
+@app.route('/corgs')
+def corgs():
+    return render_template("corgs.html")    
+
+
+    
